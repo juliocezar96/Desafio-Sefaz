45425b856dae8708c901289846ae986ce77c2c8d
juliocezar96 <juliocr.jc38@gmail.com>
2021-01-16 17:26:09 -0300

    tela de cadastro e correções

--- src/main/java/com/desafiosefaz/bean/UsuarioBean.java
+++ src/main/java/com/desafiosefaz/bean/UsuarioBean.java
@@ -1,5 +1,6 @@
 package com.desafiosefaz.bean;
 
+import com.desafiosefaz.model.Telefone;
 import com.desafiosefaz.model.Usuario;
 import com.desafiosefaz.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,65 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Component
 @ViewScoped
-@SessionScoped
 public class UsuarioBean {
 
-    private Usuario usuario = new Usuario();
+    public UsuarioBean (){
+        this.usuario = new Usuario();
+    }
+    private Usuario usuario;
+
+    public Telefone getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Telefone telefone) {
+        this.telefone = telefone;
+    }
+
+    private Telefone telefone = new Telefone();
     @Autowired
     private UsuarioService usuarioService;
+
+    public String login(){
+
+        Usuario usuario = usuarioService.login(getUsuario().getEmail(),getUsuario().getSenha());
+
+        if (usuario == null) {
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!",
+                            "Erro no Login!"));
+            return null;
+        }
+
+        setUsuario(new Usuario());
+        return "/index.jsf";
+
+    }
+    public String cadastrar (){
+
+        this.usuario.setTelefones(Arrays.asList(telefone));
+        Usuario usuario = usuarioService.cadastrar(this.usuario);
+        if (usuario == null) {
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao cadastrar!",
+                            ""));
+            return null;
+        }
+
+        setUsuario(new Usuario());
+        return "/login.jsf";
+    }
+
+
 
     public Usuario getUsuario() {
         return usuario;
@@ -28,23 +78,6 @@ public class UsuarioBean {
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    public String login(){
 
-        Usuario usuario = usuarioService.login(getUsuario().getEmail(),getUsuario().getSenha());
-
-        if (usuario == null) {
-            usuario = new Usuario();
-            FacesContext.getCurrentInstance().addMessage(
-                    null,
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!",
-                            "Erro no Login!"));
-            return null;
-        } else {
-            System.err.println(usuario.getTelefones().get(0).getNumero());
-            return "/login.jsf";
-        }
-
-
-    }
 
 }
--- src/main/java/com/desafiosefaz/service/UsuarioService.java
+++ src/main/java/com/desafiosefaz/service/UsuarioService.java
@@ -1,10 +1,12 @@
 package com.desafiosefaz.service;
 
 import com.desafiosefaz.model.Usuario;
+import com.desafiosefaz.repository.TelefoneRepository;
 import com.desafiosefaz.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 @Service
@@ -12,6 +14,8 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private TelefoneRepository telefoneRepository;
 
     public Usuario login (String email, String senha){
         Optional<Usuario> usuario = usuarioRepository.findByEmailAndSenha(email,senha);
@@ -21,5 +25,11 @@ public class UsuarioService {
             return null;
         }
 
+    }
+    public Usuario cadastrar (Usuario usuario){
+        Usuario u = usuarioRepository.save(usuario);
+        u.getTelefones().forEach(telefone -> telefone.setUsuario(u));
+        telefoneRepository.saveAll(u.getTelefones());
+        return u;
     }
 }
